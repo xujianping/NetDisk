423c4b25526008ab53fbfaded168274e2d9d4ec4
xujianping <304793680@qq.com>
2018-06-11 19:24:45 +0800

    基础目录

---
+++ src/main/java/com/xujp/NetDiskApplication.java
@@ -0,0 +1,12 @@
+package com.xujp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NetDiskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NetDiskApplication.class, args);
+	}
+}
---
+++ src/test/java/com/xujp/NetDiskApplicationTests.java
@@ -0,0 +1,16 @@
+package com.xujp;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NetDiskApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
